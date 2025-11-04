e071a2bbf1aa0ecc794ff184d959bb39af481447
Jordan-S1 <jordanshodipo1@gmail.com>
2025-11-04 14:28:08 +0000

    Updated files

--- src/main/java/org/example/model/EventType.java
+++ src/main/java/org/example/model/EventType.java
@@ -4,8 +4,6 @@ package org.example.model;
  * Enumeration representing the types of events that can occur in the simulation.
  * Each event represents a state transition for a traffic source.
  *
- * @author Jordan Shodipo
- * @version 1.0
  */
 public enum EventType {
     /**
--- src/main/java/org/example/model/SourceState.java
+++ src/main/java/org/example/model/SourceState.java
@@ -3,9 +3,6 @@ package org.example.model;
 /**
  * Enumeration representing the state of a traffic source.
  * A traffic source can be in one of two states at any given time.
- *
- * @author Jordan Shodipo
- * @version 1.0
  */
 public enum SourceState {
     /**
